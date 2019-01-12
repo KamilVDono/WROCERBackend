74a655fac996756383a136d257d8395bf4d78647
KamilVDono <kamilvdono@gmail.com>
2019-01-12 18:58:04 +0100

    Added data access service and mecz controller

---
+++ WROCERBackend/Controller/MeczController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WROCERBackend.Model.DataAccess;
+using WROCERBackend.Model.DataModel;
+
+namespace WROCERBackend.Controller
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MeczController : ControllerBase
+	{
+		private readonly IDataAccess _DataAccess;
+
+		public MeczController(IDataAccess dataAccess)
+		{
+			_DataAccess = dataAccess;
+		}
+
+		// GET: api/Mecz
+		[HttpGet]
+		public ActionResult<IEnumerable<DataMecz>> Get()
+		{
+			return Ok(_DataAccess.GetAll<DataMecz>());
+		}
+
+		// GET: api/Mecz/5
+		[HttpGet("{id}", Name = "Get")]
+		public ActionResult<DataMecz> Get(int id)
+		{
+			var item = _DataAccess.GetItem<DataMecz>(id);
+
+			if (item == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(item);
+		}
+
+		// POST: api/Mecz
+		[HttpPost]
+		public ActionResult Post([FromBody] DataMecz value)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var success = _DataAccess.AddItem(value);
+
+			if (success)
+			{
+				return CreatedAtAction("Get", value);
+			}
+			else
+			{
+				return Conflict("Can not add element");
+			}
+			
+		}
+
+		// PUT: api/Mecz/5
+		[HttpPut("{id}")]
+		public ActionResult Put(int id, [FromBody] DataMecz value)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (value == null)
+			{
+				return NotFound();
+			}
+
+			value.ID = id;
+
+			var success = _DataAccess.AddItem(value);
+
+			if (success)
+			{
+				return CreatedAtAction("Get", value);
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
+
+		// DELETE: api/ApiWithActions/5
+		[HttpDelete("{id}")]
+		public ActionResult Delete(int id)
+		{
+			var mecz = _DataAccess.GetItem<DataMecz>(id);
+
+			if (mecz == null)
+			{
+				return NotFound();
+			}
+
+			var success = _DataAccess.RemoveItem(mecz);
+
+			if (success)
+			{
+				return Ok();
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
+	}
+}
---
+++ WROCERBackend/Model/DataAccess/WrocerDataAccess.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WROCERBackend.Model.DataDirectAccess;
+using WROCERBackend.Model.DataModel;
+
+namespace WROCERBackend.Model.DataAccess
+{
+	public class WrocerDataAccess : IDataAccess
+	{
+		private readonly List<IDataDirectAccess> _DirectAccesses = new List<IDataDirectAccess>();
+
+		public WrocerDataAccess()
+		{
+			_DirectAccesses.Add(new FakeDatabase());
+		}
+
+		public WrocerDataAccess(List<IDataDirectAccess> directAccesses)
+		{
+			_DirectAccesses = directAccesses;
+		}
+
+		public IEnumerable<T> GetAll<T>() where T : AbstractDataModel
+		{
+			HashSet<T> set = new HashSet<T>();
+			_DirectAccesses.ForEach(access => set.UnionWith(access.GetAll<T>()));
+			return set;
+		}
+
+		public T GetItem<T>(long id) where T : AbstractDataModel
+		{
+			return _DirectAccesses.SelectMany(access => access.GetAll<T>()).FirstOrDefault(item => item.ID == id);
+		}
+
+		public bool AddItem<T>(T item) where T : AbstractDataModel
+		{
+			return _DirectAccesses.Any(access => access.AddItem(item));
+		}
+
+		public bool UpdateItem<T>(T item) where T : AbstractDataModel
+		{
+			return _DirectAccesses.Any(access => access.UpdateItem(item));
+		}
+
+		public bool RemoveItem<T>(T item) where T : AbstractDataModel
+		{
+			return _DirectAccesses.Any(access => access.RemoveItem(item));
+		}
+
+		public bool HasModel(Type modelType)
+		{
+			return _DirectAccesses.Any(access => access.HasModel(modelType));
+		}
+
+		public bool HasModel<T>() where T : AbstractDataModel
+		{
+			return HasModel(typeof(T));
+		}
+
+		public void AddDirectPoint(IDataDirectAccess directPoint)
+		{
+			if (_DirectAccesses.Contains(directPoint) || directPoint == null) return;
+			_DirectAccesses.Add(directPoint);
+		}
+
+		public void RemoveDirectPoint(IDataDirectAccess directPoint)
+		{
+			_DirectAccesses.Remove(directPoint);
+		}
+	}
+}
