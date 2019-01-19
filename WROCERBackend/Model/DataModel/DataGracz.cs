8f5e42450324b73a1495a8e2c210cb8cc760fe5c
KamilVDono <kamilvdono@gmail.com>
2019-01-19 20:28:45 +0100

    Added DataSklad and some data to DataGracz

--- WROCERBackend/Model/DataModel/DataGracz.cs
+++ WROCERBackend/Model/DataModel/DataGracz.cs
@@ -4,6 +4,12 @@ namespace WROCERBackend.Model.DataModel {
 	{
 		public long CzasNaBoisku { get; set; }
 
+		public int Numer { get; set; }
+
+		public DataSklad Sklad { get; set; }
+
+		public DataPozycja Pozycja { get; set; }
+
 		public DataZawodnik Zawodnik{ get; set; } 
 
 		public DataRaport Raport{ get; set; } 
---
+++ WROCERBackend/Model/DataModel/DataSklad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WROCERBackend.Model.DataModel
+{
+	public class DataSklad : AbstractDataModel
+	{
+		public string Nazwa { get; set; }
+	}
+}
