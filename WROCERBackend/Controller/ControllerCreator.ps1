85dc64938cc84e71f3f2dc6bebe4bf04c2da9e64
KamilVDono <kamilvdono@gmail.com>
2019-01-20 14:03:09 +0100

    Generate left controllers with template

--- WROCERBackend/Controller/ControllerCreator.ps1
+++ WROCERBackend/Controller/ControllerCreator.ps1
@@ -1,60 +1,70 @@
-﻿$typ = "DataDruzyna"
-$controllerName = "Druzyna"
-$class = "using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using WROCERBackend.Model.DataModel;
+﻿$pathes = get-childitem "..\Model\DataModel" -recurse
+Foreach($path in $pathes){
+    If($path -like "IData*" -or $path -like "Abstract*" -or $path -like "*.ps1"){
+        continue
+    }
+    $className = ($path -replace ".cs" , "")
+    $var = "public DbSet<" + $className + "> "+$className+"s { get; set; }"
+    
+    $typ = ($path -replace ".cs" , "")
+    $controllerName = ($typ -replace "Data" , "")
+    $class = "using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using WROCERBackend.Model.DataModel;
 
-namespace WROCERBackend.Controller
-{
-	[Route(""api/[controller]"")]
-	[ApiController]
-	public class "+$controllerName+"Controller : BaseController<$typ>
-	{
-		// GET: api/$controllerName
-		[HttpGet]
-		public ActionResult<IEnumerable<$typ>> Get()
-		{
-			return Ok(GetAll());
-		}
+    namespace WROCERBackend.Controller
+    {
+	    [Route(""api/[controller]"")]
+	    [ApiController]
+	    public class "+$controllerName+"Controller : BaseController<$typ>
+	    {
+		    // GET: api/$controllerName
+		    [HttpGet]
+		    public ActionResult<IEnumerable<$typ>> Get()
+		    {
+			    return Ok(GetAll());
+		    }
 
-		// GET: api/$controllerName/5
-		[HttpGet(""{id}"", Name = ""Get$typ"")]
-		public ActionResult<$typ> Get(int id)
-		{
-			return TryGet(id);
-		}
+		    // GET: api/$controllerName/5
+		    [HttpGet(""{id}"", Name = ""Get$typ"")]
+		    public ActionResult<$typ> Get(int id)
+		    {
+			    return TryGet(id);
+		    }
 
-		// POST: api/$controllerName
-		[HttpPost]
-		public ActionResult Post([FromBody] $typ value)
-		{
-			return TryPost(value);
-		}
+		    // POST: api/$controllerName
+		    [HttpPost]
+		    public ActionResult Post([FromBody] $typ value)
+		    {
+			    return TryPost(value);
+		    }
 
-		// PUT: api/$controllerName/5
-		[HttpPut(""{id}"")]
-		public ActionResult Put(int id, [FromBody] $typ value)
-		{
-			return TryPut(id, value);
-		}
+		    // PUT: api/$controllerName/5
+		    [HttpPut(""{id}"")]
+		    public ActionResult Put(int id, [FromBody] $typ value)
+		    {
+			    return TryPut(id, value);
+		    }
 
-		// DELETE: api/$controllerName/5
-		[HttpDelete(""{id}"")]
-		public ActionResult Delete(int id)
-		{
-			return TryDelete(id);
-		}
-	}
-}"
+		    // DELETE: api/$controllerName/5
+		    [HttpDelete(""{id}"")]
+		    public ActionResult Delete(int id)
+		    {
+			    return TryDelete(id);
+		    }
+	    }
+    }"
 
-$path = $controllerName + "Controller.cs"
+    $path2 = $controllerName + "Controller.cs"
 
-If(Test-Path $path -PathType Leaf)
-{
-    Write-Host "$path File Exists"
-}
-Else
-{
-    $class | Out-File -FilePath $path
-    Write-Host "$path File Created"
+    If(Test-Path $path2 -PathType Leaf)
+    {
+        Write-Host "$path2 File Exists"
+    }
+    Else
+    {
+        $class | Out-File -FilePath $path2
+        Write-Host "$path2 File Created"
+    }
+
 }
