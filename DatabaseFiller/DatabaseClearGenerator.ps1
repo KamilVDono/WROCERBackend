5c840540a4bb93399b5fb8f0a6b8f9994f77886b
KamilVDono <kamilvdono@gmail.com>
2019-01-21 19:36:17 +0100

    Clearing database

--- DatabaseFiller/DatabaseClearGenerator.ps1
+++ DatabaseFiller/DatabaseClearGenerator.ps1
@@ -1,1 +1,46 @@
 ﻿$pathes = get-childitem "..\WROCERBackend\Model\DataModel" -recurse
+
+$header1 = "public static void ClearDatabase()
+		{
+"
+$content1 = ""
+Foreach($path in $pathes){
+    If($path -like "IData*" -or $path -like "Abstract*" -or $path -like "*.ps1"){
+        continue
+    }
+    
+    $className = ($path -replace ".cs" , "")
+    $var = "public DbSet<" + $className + "> "+$className+"s { get; set; }"
+    
+    $typ = ($path -replace ".cs" , "")
+    $controllerName = ($typ -replace "Data" , "")
+    $content1 = $content1 + "
+    ClearDatabase" + $controllerName + "();"
+}
+$bottom1 = "
+}"
+$fn1 = $header1+$content1+$bottom1
+Write-Host $fn1
+
+$fn2 = ""
+Foreach($path in $pathes){
+    If($path -like "IData*" -or $path -like "Abstract*" -or $path -like "*.ps1"){
+        continue
+    }
+    $className = ($path -replace ".cs" , "")
+    $var = "public DbSet<" + $className + "> "+$className+"s { get; set; }"
+    
+    $typ = ($path -replace ".cs" , "")
+    $controllerName = ($typ -replace "Data" , "")
+    $fn2 = $fn2 +"
+    " + "private static void ClearDatabase"+$controllerName+"()
+		{
+			var items = Deserialize<$typ>(BaseUri + ""$controllerName"");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, ""$controllerName"");
+			}
+		}"
+}
+Write-Host $fn2
+
--- DatabaseFiller/Program.cs
+++ DatabaseFiller/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+using WROCERBackend.Model.DataModel;
 
 namespace DatabaseFiller
 {
@@ -19,7 +24,6 @@ namespace DatabaseFiller
 			}
 
 			ClearDatabase();
-
 			Console.ReadLine();
 		}
 
@@ -36,17 +40,142 @@ namespace DatabaseFiller
 			}
 		}
 
-		public static void ClearDatabase()
+		public static IEnumerable<AbstractDataModel> Deserialize<T>(string uri) where T : AbstractDataModel
 		{
-
+			var json = Get(uri);
+			var o =  JsonConvert.DeserializeObject<List<T>>(json);
+			return o;
 		}
 
-		public static void DeleteItem(string uri, int id, string type)
+		public static void DeleteItem(string uri, long id, string type)
 		{
-			WebRequest request = WebRequest.Create(uri+$"{type}/{id}");
+			WebRequest request = WebRequest.Create(uri + $"{type}/{id}");
 			request.Method = "DELETE";
 
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		}
+
+		public static void ClearDatabase()
+		{
+
+			ClearDatabaseDruzyna();
+			ClearDatabaseGracz();
+			ClearDatabaseMecz();
+			ClearDatabasePozycja();
+			ClearDatabaseRaport();
+			ClearDatabaseSezon();
+			ClearDatabaseSklad();
+			ClearDatabaseSytuacjaMeczowa();
+			ClearDatabaseSytuacjaTyp();
+			ClearDatabaseUzytkownik();
+			ClearDatabaseUzytkownikTyp();
+			ClearDatabaseZawodnik();
+			ClearDatabaseZmiana();
+		}
+
+		private static void ClearDatabaseDruzyna()
+		{
+			var items = Deserialize<DataDruzyna>(BaseUri + "Druzyna");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Druzyna");
+			}
+		}
+		private static void ClearDatabaseGracz()
+		{
+			var items = Deserialize<DataGracz>(BaseUri + "Gracz");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Gracz");
+			}
+		}
+		private static void ClearDatabaseMecz()
+		{
+			var items = Deserialize<DataMecz>(BaseUri + "Mecz");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Mecz");
+			}
+		}
+		private static void ClearDatabasePozycja()
+		{
+			var items = Deserialize<DataPozycja>(BaseUri + "Pozycja");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Pozycja");
+			}
+		}
+		private static void ClearDatabaseRaport()
+		{
+			var items = Deserialize<DataRaport>(BaseUri + "Raport");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Raport");
+			}
+		}
+		private static void ClearDatabaseSezon()
+		{
+			var items = Deserialize<DataSezon>(BaseUri + "Sezon");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Sezon");
+			}
+		}
+		private static void ClearDatabaseSklad()
+		{
+			var items = Deserialize<DataSklad>(BaseUri + "Sklad");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Sklad");
+			}
+		}
+		private static void ClearDatabaseSytuacjaMeczowa()
+		{
+			var items = Deserialize<DataSytuacjaMeczowa>(BaseUri + "SytuacjaMeczowa");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "SytuacjaMeczowa");
+			}
+		}
+		private static void ClearDatabaseSytuacjaTyp()
+		{
+			var items = Deserialize<DataSytuacjaTyp>(BaseUri + "SytuacjaTyp");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "SytuacjaTyp");
+			}
+		}
+		private static void ClearDatabaseUzytkownik()
+		{
+			var items = Deserialize<DataUzytkownik>(BaseUri + "Uzytkownik");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Uzytkownik");
+			}
+		}
+		private static void ClearDatabaseUzytkownikTyp()
+		{
+			var items = Deserialize<DataUzytkownikTyp>(BaseUri + "UzytkownikTyp");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "UzytkownikTyp");
+			}
+		}
+		private static void ClearDatabaseZawodnik()
+		{
+			var items = Deserialize<DataZawodnik>(BaseUri + "Zawodnik");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Zawodnik");
+			}
+		}
+		private static void ClearDatabaseZmiana()
+		{
+			var items = Deserialize<DataZmiana>(BaseUri + "Zmiana");
+			foreach (var item in items)
+			{
+				DeleteItem(BaseUri, item.ID, "Zmiana");
+			}
 		}
 	}
 }
